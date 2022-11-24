85b5442a3d1c710e9f61be9163f40de9c2c397d1
Herbstmeier <moritz.herbstmeier@gmx.de>
2022-11-24 05:17:03 +0100

    backend: added data models, controllers and http request routes

--- Server/models/db.js
+++ Server/models/db.js
@@ -1,16 +1,16 @@
-const mariadb = require('mariadb/callback');
-const dbConfig = require("../config/db.config.js");
+import { createConnection } from 'mariadb/callback';
+import { HOST, USER, PASSWORD } from "../config/db.config.js";
 
 // Create a new connection
-const connection = await mariadb.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
+const connection = await createConnection({
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
 });
 
 connection.connect(error => {
     if (error) throw error;
     console.log("Successfully connected to the database.");
-  });
-  
-  module.exports = connection;
+});
+
+export default connection;
--- Server/models/set.model.js
+++ Server/models/set.model.js
@@ -1,9 +1,91 @@
 import sql from "./db.js";
 
 class Set {
-    constructor(SetterId, BoulderId, Date) {
-        this.SetterId = SetterId;
-        this.BoulderId = BoulderId;
-        this.Date = Date;
+    constructor(setterId, boulderId, date) {
+        this.setterId = setterId;
+        this.boulderId = boulderId;
+        this.date = date;
+    }
+
+    create(newSet, result) {
+        sql.query("insert into sets set ?", newSet, (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+                return;
+            }
+
+            result(null, newSet);
+        })
+    }
+
+    getByBoulder(id, result) {
+        sql.query("select * from sets where boulderId = ?", id, (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+                return;
+            }
+
+            if (res.length) {
+                console.log("found sets: ", res);
+                result(null, res);
+                return;
+            }
+
+            result({ kind: "not_found" }, null);
+        })
+    }
+
+    getBySetter(id, result) {
+        sql.query("select * from sets where setterId = ?", id, (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+                return;
+            }
+
+            if (res.length) {
+                console.log("found sets: ", res);
+                result(null, res);
+                return;
+            }
+
+            result({ kind: "not_found" }, null);
+        })
+    }
+
+    delete(sid, bid, result) {
+        sql.query("delete * from sets where setterId = ?, boulderId = ?", [sid, bid], (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+                return;
+            }
+
+            if (res.affectedRows == 0) {
+                result({ kind: "not_found" }, null);
+                return;
+            }
+
+            result(null, res);
+        })
+    }
+
+    deleteByBoulder(id, result) {
+        sql.query("delete * from sets where boulderId = ?", id, (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+                return;
+            }
+
+            if (res.affectedRows == 0) {
+                result({ kind: "not_found" }, null);
+                return;
+            }
+
+            result(null, res)
+        })
     }
 }
